944c33ed2947b3a91281242be718bb180295abd5
ArmandoEduardoSanchezHerrera <163336913+ArmandoEduardoSanchezHerrera@users.noreply.github.com>
2024-07-02 16:40:24 -0600

    Add files via upload
    La tienda ya puede comprar por el boton de carrito y en detalles, con session destroy puedes borrar el array de arriba que lo creo para que vieras como se actualiza y que no solo le das una vez a añadir unicamente sino que puedes añadir asi mas piezas de ese mismo producto

--- Tienda_Jamones/details.php
+++ Tienda_Jamones/details.php
@@ -1,7 +1,8 @@
 <?php
 require 'config/config.php';
 require 'config/database.php';
-$db = new Database(); 
+
+$db = new Database();
 $con = $db->conectar();
 
 $id = isset($_GET['id']) ? $_GET['id'] : '';
@@ -13,10 +14,10 @@ if($id == '' || $token == '') {
 } else {
     $token_tmp = hash_hmac('sha1', $id, KEY_TOKEN);
 
-    if($token == $token_tmp){
+    if($token == $token_tmp) {
         $sql = $con->prepare("SELECT count(id) FROM productos WHERE id=? AND activo=1");
         $sql->execute([$id]);
-        if($sql->fetchColumn() > 0){
+        if($sql->fetchColumn() > 0) {
             $sql = $con->prepare("SELECT nombre, descripcion, precio, descuento FROM productos WHERE id=? AND activo=1 LIMIT 1");
             $sql->execute([$id]);
             $row = $sql->fetch(PDO::FETCH_ASSOC);
@@ -35,14 +36,16 @@ if($id == '' || $token == '') {
             }
 
             $imagenes = array();
-            $dir = opendir($dir_images);
-            if ($dir) {
-                while(($archivo = readdir($dir)) !== false){
-                    if($archivo != 'principal.jpg' && (strpos($archivo, 'jpg') !== false || strpos($archivo, 'jpeg') !== false)){
-                        $imagenes[] = $dir_images . $archivo;
+            if(file_exists($dir_images)) {
+                $dir = opendir($dir_images);
+                if ($dir) {
+                    while(($archivo = readdir($dir)) !== false) {
+                        if($archivo != 'principal.jpg' && (strpos($archivo, 'jpg') !== false || strpos($archivo, 'jpeg') !== false)) {
+                            $imagenes[] = $dir_images . $archivo;
+                        }
                     }
+                    closedir($dir);  // Cambio realizado aquí
                 }
-                closedir($dir);
             }
         } else {
             echo 'No se encontraron productos activos con ese ID.';
@@ -51,106 +54,135 @@ if($id == '' || $token == '') {
     } else {
         echo 'Error al procesar la petición';
         exit;
+      }
     }
-}
-
-$sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
-$sql->execute();
-$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>tienda_jamones</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
-    rel="stylesheet" 
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
-    crossorigin="anonymous">
-    <link href="css/estilos.css" rel="stylesheet">
-</head>
-<body>
-<header>
-    <div class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a href="#" class="navbar-brand">
-        <strong>Tienda Jamones</strong>
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarHeader">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a href="#" class="nav-link active">Catalogo</a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">Contacto</a>
-          </li>
-        </ul>  
-        <a href="carrito.php" class="btn btn-primary">Carrito</a>    
+    $sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
+    $sql->execute();
+    $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+       
+    ?>
+    
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>tienda_jamones</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" 
+        crossorigin="anonymous">
+        <link href="css/estilos.css" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+        <div class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a href="#" class="navbar-brand">
+            <strong>Tienda Jamones</strong>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+    
+          <div class="collapse navbar-collapse" id="navbarHeader">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a href="#" class="nav-link active">Catalogo</a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">Contacto</a>
+              </li>
+            </ul> 
+   
+            <a href="carrito.php" class="btn btn-primary">
+              Carrito <span id="num_cart" class="badge bg-secondary" >
+                <?php echo $num_cart; ?></span>
+            </a>    
+    
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</header>
-<!--Contenido-->
-
-<main>
-    <div class="container">
-            <div class="row">
-                <div class="col-md-6 order-md-1">
-                <div id="carouselImages" class="carousel slide" data-bs-ride="carousel">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <img src="<?php echo $rutaImg; ?>" class="d-block w-100">
+    </header>
+    <!--Contenido-->
+    
+    <main>
+        <div class="container">
+                <div class="row">
+                    <div class="col-md-6 order-md-1">
+                    <div id="carouselImages" class="carousel slide" data-bs-ride="carousel">
+                      <div class="carousel-inner">
+                        <div class="carousel-item active">
+                          <img src="<?php echo $rutaImg; ?>" class="d-block w-100">
+                        </div>
+    
+                        <?php foreach($imagenes as $img) { ?>
+                        <div class="carousel-item">
+                            <img src="<?php echo $img; ?>" class="d-block w-100">
+                        </div>
+                        <?php } ?>
+                      </div>
+                      <button class="carousel-control-prev" type="button" data-bs-target="#carouselImages" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                      </button>
+                      <button class="carousel-control-next" type="button" data-bs-target="#carouselImages" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                      </button>
+                    </div>                  
                     </div>
-
-                    <?php foreach($imagenes as $img) { ?>
-                    <div class="carousel-item">
-                        <img src="<?php echo $img; ?>" class="d-block w-100">
-                    </div>
+                <div class="col-md-6 order-md-2">
+                    <h2><?php echo $nombre; ?></h2>
+    
+                    <?php if ($descuento > 0){ ?>
+                        <p><del><?php echo MONEDA . number_format($precio, 2, '.', ','); ?></del></p>
+                        <h2>  
+                        <?php echo MONEDA . number_format($precio_desc, 2, '.', ','); ?></h2>
+                        <small class="text-success"><?php echo $descuento; ?>% descuento </small>
+                    <?php } else { ?>
+                    <h2> <?php echo MONEDA . number_format($precio, 2, '.', ','); ?></h2>
                     <?php } ?>
-                  </div>
-                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselImages" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                  </button>
-                  <button class="carousel-control-next" type="button" data-bs-target="#carouselImages" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                  </button>
-                </div>                  
-                </div>
-            <div class="col-md-6 order-md-2">
-                <h2><?php echo $nombre; ?></h2>
+                    
+                    <p class="lead">
+                        <?php echo $descripcion; ?>
+                    </p>
+                    <div class="d-grid gap-3 col-10 mx-auto">
+                        <button class="btn btn-primary" type="button">Comprar ahora</button>
 
-                <?php if ($descuento > 0){ ?>
-                    <p><del><?php echo MONEDA . number_format($precio, 2, '.', ','); ?></del></p>
-                    <h2>  
-                    <?php echo MONEDA . number_format($precio_desc, 2, '.', ','); ?></h2>
-                    <small class="text-success"><?php echo $descuento; ?>% descuento </small>
-                <?php } else { ?>
-                <h2> <?php echo MONEDA . number_format($precio, 2, '.', ','); ?></h2>
-                <?php } ?>
-                
-                <p class="lead">
-                    <?php echo $descripcion; ?>
-                </p>
-                <div class="d-grid gap-3 col-10 mx-auto">
-                    <button class="btn btn-primary" type="button">Comprar ahora</button>
-                    <button class="btn btn-outline-primary" type="button">Agregar al carrito</button>
-                </div> 
+                        <button class="btn btn-outline-primary" type="button" onclick="addProducto(<?php echo 
+                        $id; ?>, '<?php echo $token_tmp;?>')">Agregar al carrito</button>
+                    </div> 
+                </div>
             </div>
         </div>
-    </div>
-</main>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
-crossorigin="anonymous"></script>
-</body>
-</html>
+    </main>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
+    crossorigin="anonymous"></script>
+    
+    <script>
+        function addProducto(id, token){
+          let url = 'clases/carrito.php'
+          let formData = new FormData()
+          formData.append('id', id)
+          formData.append('token', token)
+    
+          fetch(url, {
+            method: 'POST',
+            body: formData,
+            mode: 'cors'
+          }).then(response => response.json())
+    
+          .then(data =>{
+            if(data.ok){
+              let elemento = document.getElementById("num_cart")
+              elemento.innerHTML =data.numero
+            }
+          })
+        }
+    </script>
+    </body>
+    </html>
+    
--- Tienda_Jamones/index.php
+++ Tienda_Jamones/index.php
@@ -1,6 +1,4 @@
 <?php
-
-
 require 'config/config.php';
 require 'config/database.php';
 $db = new Database(); 
@@ -9,6 +7,10 @@ $con = $db->conectar();
 $sql = $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
 $sql->execute();
 $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+//session_destroy();
+
+print_r($_SESSION);
 
 ?>
 
@@ -44,7 +46,10 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
             <a href="#" class="nav-link">Contacto</a>
           </li>
         </ul>  
-        <a href="carrito.php" class="btn btn-primary">Carrito</a>    
+        <a href="carrito.php" class="btn btn-primary">
+          Carrito <span id="num_cart" class="badge bg-secondary" >
+            <?php echo $num_cart; ?></span>
+        </a> 
       </div>
     </div>
   </div>
@@ -69,17 +74,20 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
              <div class="d-flex justify-content-between align-items-center">  
              <div class="btn-group">
                 
-                <a href="details.php?id=<?php echo $row['id']; ?>&token=<?php echo hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>" class="btn btn-primary"> Detalles </a>
+                <a href="details.php?id=<?php echo $row['id']; ?>&token=<?php echo 
+                hash_hmac('sha1', $row['id'], KEY_TOKEN); ?>" class="btn 
+                btn-primary"> Detalles </a>
                 </div>
-                <a href="#" class="btn btn-success">Agregar</a>
+                <button class="btn btn-outline-success" type="button" onclick="addProducto
+                (<?php echo $row['id']; ?>, '<?php echo hash_hmac('sha1', $row['id'], 
+                KEY_TOKEN); ?>')">Agregar al carrito</button>
+
               </div>
               </div>
              </div>
           </div>
           <?php } ?>
-        </div>
-        
-      
+        </div>   
       </div>
 </main>
 
@@ -87,6 +95,27 @@ $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" 
 crossorigin="anonymous"></script>
-    
+
+<script>
+    function addProducto(id, token){
+      let url = 'clases/carrito.php'
+      let formData = new FormData()
+      formData.append('id', id)
+      formData.append('token', token)
+
+      fetch(url, {
+        method: 'POST',
+        body: formData,
+        mode: 'cors'
+      }).then(response => response.json())
+
+      .then(data =>{
+        if(data.ok){
+          let elemento = document.getElementById("num_cart")
+          elemento.innerHTML =data.numero
+        }
+      })
+    }
+</script>
 </body>
 </html>
